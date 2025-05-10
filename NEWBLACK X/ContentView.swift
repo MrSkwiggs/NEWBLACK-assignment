818d6c71649108e3440e743abf88136185b91747
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-10 09:40:33 +0200

    🎉 Create Xcode project

---
+++ NEWBLACK X/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NEWBLACK X
+//
+//  Created by Dorian on 10/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NEWBLACK X/NEWBLACK_XApp.swift
@@ -0,0 +1,17 @@
+//
+//  NEWBLACK_XApp.swift
+//  NEWBLACK X
+//
+//  Created by Dorian on 10/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct NEWBLACK_XApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
