69636d2dee428aba2860731871f9806507570e9f
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-12 10:07:58 +0200

    ♻️ Show and navigate to launch views

--- NEWBLACK X/Scenes/Launches/LaunchesView.swift
+++ NEWBLACK X/Scenes/Launches/LaunchesView.swift
@@ -9,7 +9,23 @@ import SwiftUI
 
 struct LaunchesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section("Upcoming") {
+                ForEach(0..<5) { launch in
+                    NavigationLink("Upcoming Launch \(launch)") {
+                        LaunchView(launch: "Upcoming Launch \(launch)")
+                    }
+                }
+            }
+
+            Section("Past") {
+                ForEach(0..<10) { launch in
+                    NavigationLink("Past Launch \(launch)") {
+                        LaunchView(launch: "Past Launch \(launch)")
+                    }
+                }
+            }
+        }
     }
 }
 
