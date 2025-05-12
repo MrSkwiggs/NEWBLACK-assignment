5ea49b4071cdc5a9ed90380e4a741ed8155cb022
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-12 09:48:43 +0200

    ✨ Create Launches view

---
+++ NEWBLACK X/Scenes/Launches/LaunchesView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchesView.swift
+//  NEWBLACK X
+//
+//  Created by Dorian on 12/05/2025.
+//
+
+import SwiftUI
+
+struct LaunchesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchesView()
+}
