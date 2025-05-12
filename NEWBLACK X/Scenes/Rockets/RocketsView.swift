11b01ba8d46913a6e7803a2cc636b63d5cc66815
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-12 09:49:22 +0200

    ✨ Create Rockets view

---
+++ NEWBLACK X/Scenes/Rockets/RocketsView.swift
@@ -0,0 +1,18 @@
+//
+//  RocketsView.swift
+//  NEWBLACK X
+//
+//  Created by Dorian on 12/05/2025.
+//
+
+import SwiftUI
+
+struct RocketsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RocketsView()
+}
