61948fb0c5c337b020f6bff750f0328f0041465e
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-13 10:56:45 +0200

    ✨ Create Launch DTO

---
+++ Packages/Core/Sources/API/DTOs/LaunchDTO.swift
@@ -0,0 +1,72 @@
+//
+//  LaunchDTO.swift
+//  Core
+//
+//  Created by Dorian on 13/05/2025.
+//
+
+import Foundation
+
+public struct LaunchDTO: DTO {
+    public let id: String
+    public let date: Date
+    public let isUpcoming: Bool
+    public let name: String
+    public let launchpadID: String
+    public let links: LinksDTO
+    public let rocketID: String
+    public let isSuccess: Bool?
+    public let failures: [FailureDTO]?
+    public let details: String?
+
+    public init(
+        id: String,
+        date: Date,
+        isUpcoming: Bool,
+        name: String,
+        launchpadID: String,
+        links: LinksDTO,
+        rocketID: String,
+        isSuccess: Bool?,
+        failures: [FailureDTO]?,
+        details: String?
+    ) {
+        self.id = id
+        self.date = date
+        self.isUpcoming = isUpcoming
+        self.name = name
+        self.launchpadID = launchpadID
+        self.links = links
+        self.rocketID = rocketID
+        self.isSuccess = isSuccess
+        self.failures = failures
+        self.details = details
+    }
+
+    public enum Field: String, CodingKey, Sendable, Hashable, CaseIterable, Equatable {
+        case id
+        case date
+        case isUpcoming = "upcoming"
+        case name
+        case launchpadID = "launchpad"
+        case links
+        case rocketID = "rocket"
+        case isSuccess = "success"
+        case failures
+        case details
+    }
+
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: Field.self)
+        self.id = try container.decode(.id)
+        self.date = try container.decode(.date)
+        self.isUpcoming = try container.decode(.isUpcoming)
+        self.name = try container.decode(.name)
+        self.launchpadID = try container.decode(.launchpadID)
+        self.links = try container.decode(.links)
+        self.rocketID = try container.decode(.rocketID)
+        self.isSuccess = try container.decodeIfPresent(.isSuccess)
+        self.failures = try container.decodeIfPresent(.failures)
+        self.details = try container.decodeIfPresent(.details)
+    }
+}
