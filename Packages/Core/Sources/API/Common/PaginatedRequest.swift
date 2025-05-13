7cc4f276e96a2db8fd7e22375289eff9b8092a38
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-13 17:33:41 +0200

    ✨ Create PaginatedRequest

---
+++ Packages/Core/Sources/API/Common/PaginatedRequest.swift
@@ -0,0 +1,13 @@
+//
+//  PaginatedRequest.swift
+//  Core
+//
+//  Created by Dorian on 13/05/2025.
+//
+
+import Foundation
+import Networking
+
+protocol PaginatedRequest: Request, BodyProvider, Sendable where Response == Paginated<Item>, ProvidedBody == Query<Item> {
+    associatedtype Item: DTO
+}
