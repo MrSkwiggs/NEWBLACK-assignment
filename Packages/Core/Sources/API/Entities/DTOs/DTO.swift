972837f94f4309fa4dee2bf58fa905e50b829f6e
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-13 13:01:41 +0200

    🚚 Rearrange folders

--- Packages/Core/Sources/API/Entities/DTOs/DTO.swift
+++ Packages/Core/Sources/API/Entities/DTOs/DTO.swift
@@ -11,9 +11,9 @@ public protocol APIModel: Sendable, Decodable, Equatable, Hashable {}
 
 
 /// A type that represents an API Model.
-public protocol DTO: APIModel, Identifiable where Field.RawValue == String {
+public protocol DTO: APIModel, Identifiable where Field.RawValue == String, Field.AllCases == [Field] {
     /// A type that represents fields of the Model.
     associatedtype Field: RawRepresentable, Sendable, Hashable, CaseIterable, Equatable
 
-//    typealias Filter = Query<Self>.Filter
+    typealias Filter = Query<Self>.Filter
 }
