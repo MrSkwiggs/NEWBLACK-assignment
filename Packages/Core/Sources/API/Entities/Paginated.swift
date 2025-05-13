76fe4f72c4796302b390a97187c2cd930428682d
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-13 17:33:50 +0200

    ✨ Create Paginated response

---
+++ Packages/Core/Sources/API/Entities/Paginated.swift
@@ -0,0 +1,14 @@
+//
+//  Paginated.swift
+//  Core
+//
+//  Created by Dorian on 13/05/2025.
+//
+
+import Foundation
+
+public struct Paginated<Item: Sendable & Decodable>: Sendable, Decodable {
+    public let docs: [Item]
+    public let page: Int
+    public let pageSize: Int
+}
