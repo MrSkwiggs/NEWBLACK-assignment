25b47ecd222a14f90562142bd676a59a3e6328a4
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-13 13:46:21 +0200

    ✨ Create API object

---
+++ Packages/Core/Sources/API/API.swift
@@ -0,0 +1,24 @@
+//
+//  API.swift
+//  Core
+//
+//  Created by Dorian on 13/05/2025.
+//
+
+import Foundation
+import Networking
+
+public final class API: Sendable {
+
+    private let requestPerformer: RequestPerformer
+
+    public init() {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .secondsSince1970
+        self.requestPerformer = RequestPerformer(decoder: decoder)
+    }
+
+    public func send<R: Request>(_ request: R) async throws -> R.Response {
+        try await requestPerformer.send(request)
+    }
+}
