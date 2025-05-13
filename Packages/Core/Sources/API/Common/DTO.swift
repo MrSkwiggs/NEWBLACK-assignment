629b0f41af0592c64a41def2040fcbc6ef7e3142
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-13 10:56:15 +0200

    ♻️ Split up protocol

--- Packages/Core/Sources/API/Common/DTO.swift
+++ Packages/Core/Sources/API/Common/DTO.swift
@@ -5,9 +5,13 @@
 //  Created by Dorian on 11/05/2025.
 //
 
+import Foundation
+
+public protocol APIModel: Sendable, Decodable, Equatable, Hashable {}
+
 
 /// A type that represents an API Model.
-public protocol DTO: Decodable, Sendable, Identifiable, Equatable where Field.RawValue == String {
+public protocol DTO: APIModel, Identifiable where Field.RawValue == String {
     /// A type that represents fields of the Model.
     associatedtype Field: RawRepresentable, Sendable, Hashable, CaseIterable, Equatable
 
