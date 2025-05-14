4baa394c4daa9eb88f090461286610d7475f207a
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-14 15:21:44 +0200

    ✨ Create Launchpad view

---
+++ NEWBLACK X/Scenes/Launches/LaunchpadView.swift
@@ -0,0 +1,45 @@
+//
+//  LaunchpadView.swift
+//  NEWBLACK X
+//
+//  Created by Dorian on 14/05/2025.
+//
+
+import SwiftUI
+import API
+
+struct LaunchpadView: View {
+    let launchpad: Launchpad
+
+    var body: some View {
+        StickyHeaderList {
+            AsyncGallery(images: launchpad.imageURLs)
+        } content: {
+            Text(launchpad.name)
+                .bold()
+            Text(launchpad.fullName)
+
+            Section {
+                Text(launchpad.details)
+                LabeledContent {
+                    Text(launchpad.launchCount.description)
+                } label: {
+                    Text("Launches")
+                }
+                LabeledContent {
+                    Text(launchpad.status.rawValue)
+                        .foregroundStyle(launchpad.status == .active ? .green : .purple)
+                } label: {
+                    Text("Status")
+                }
+            }
+        }
+    }
+}
+
+import Mocks
+#Preview {
+    NavigationStack {
+        LaunchpadView(launchpad: .omega)
+    }
+}
