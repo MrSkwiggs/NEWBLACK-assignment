bbdb7de696b4a7ddf73d705a67a1a322df8d3d79
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-14 20:47:14 +0200

    ✨ Create WebView representable

---
+++ NEWBLACK X/Views/WebView.swift
@@ -0,0 +1,22 @@
+//
+//  WebView.swift
+//  NEWBLACK X
+//
+//  Created by Dorian on 14/05/2025.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+}
