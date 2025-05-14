5f53a6e2530cc26e74f1e4a270f0c3b877100d04
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-14 10:22:20 +0200

    ✨ Create LaunchpadDTO

---
+++ Packages/Core/Sources/API/Entities/DTOs/LaunchpadDTO.swift
@@ -0,0 +1,64 @@
+//
+//  LaunchpadDTO.swift
+//  Core
+//
+//  Created by Dorian on 14/05/2025.
+//
+
+import Foundation
+
+public struct LaunchpadDTO: DTO {
+    public enum Field: String, CodingKey, DTOField {
+        case id
+        case name
+        case imageURLs = "images"
+        case location
+        case details
+        case status
+        case launches
+    }
+
+    public let id: String
+    public let name: String
+    public let fullName: String
+    public let imageURLs: [URL]
+    public let details: String
+    public let status: Status
+    public let launchCount: Int
+
+    public init(
+        id: String,
+        name: String,
+        imageURLs: [URL] = [],
+        details: String,
+        status: Status,
+        launchCount: Int = 0
+    ) {
+        self.id = id
+        self.name = name
+        self.fullName = name
+        self.imageURLs = imageURLs
+        self.details = details
+        self.status = status
+        self.launchCount = launchCount
+    }
+
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: Field.self)
+        self.id = try container.decode(.id)
+        self.name = try container.decode(.name)
+        self.fullName = try container.decode(.name)
+        let imageContainer = try container.nestedContainer(keyedBy: DynamicCodingKey.self, forKey: .imageURLs)
+        imageURLs = try imageContainer.decode("large")
+        details = try container.decode(.details)
+        status = try container.decode(.status)
+        launchCount = try container.decode([String].self, forKey: .launches).count
+    }
+}
+
+public extension LaunchpadDTO {
+    enum Status: String, APIModel {
+        case active
+        case retired
+    }
+}
