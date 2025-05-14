e6321336104f812d2819640c9e46f6a9576a9fbb
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-14 16:43:16 +0200

    ✨ Add next page

--- Packages/Core/Sources/API/Entities/Paginated.swift
+++ Packages/Core/Sources/API/Entities/Paginated.swift
@@ -11,10 +11,12 @@ public struct Paginated<Item: Sendable & Decodable>: Sendable, Decodable {
     public let docs: [Item]
     public let page: Int
     public let pageSize: Int
+    public let nextPage: Int?
 
     enum CodingKeys: String, CodingKey {
         case docs
         case page
         case pageSize = "limit"
+        case nextPage = "nextPage"
     }
 }
