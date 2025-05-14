0269451c4752ab6180e5646cec5c935f4b2b9700
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-14 10:41:03 +0200

    🐛 FIx decoding

--- Packages/Core/Sources/API/Entities/Paginated.swift
+++ Packages/Core/Sources/API/Entities/Paginated.swift
@@ -11,4 +11,10 @@ public struct Paginated<Item: Sendable & Decodable>: Sendable, Decodable {
     public let docs: [Item]
     public let page: Int
     public let pageSize: Int
+
+    enum CodingKeys: String, CodingKey {
+        case docs
+        case page
+        case pageSize = "limit"
+    }
 }
