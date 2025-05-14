22c69820b9a1a5e407fabe74a14c74143cc71205
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-14 15:22:38 +0200

    💄 Allow styling header size

--- NEWBLACK X/Views/StickyHeaderList.swift
+++ NEWBLACK X/Views/StickyHeaderList.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct StickyHeaderList<Header: View, Content: View>: View {
 
+    @Environment(\.stickyHeaderListStyle)
+    var style: StickyHeaderListStyle
+
     let header: Header
     let content: Content
 
@@ -26,32 +29,63 @@ struct StickyHeaderList<Header: View, Content: View>: View {
         self.content = content()
     }
 
+    private let scalePadding: CGFloat = 20
+
     var body: some View {
         GeometryReader { reader in
-                List {
-                    Section {
-                    } header: {
-                        header
-                            .listRowInsets(.init(top: -20, leading: 0, bottom: 0, trailing: 0))
-                            .frame(width: reader.size.width, height: 320)
-                            .scaleEffect(x: scaleEffect, y: scaleEffect, anchor: .bottom)
-                    }
-                    content
+            List {
+                Section {
+                } header: {
+                    header
+                        .listRowInsets(.init(top: -20, leading: 0, bottom: 0, trailing: 0))
+                        .frame(width: reader.size.width, height: style.headerHeight + scalePadding)
+                        .scaleEffect(x: scaleEffect, y: scaleEffect, anchor: .bottom)
+                        .textCase(nil)
                 }
-                .onScrollGeometryChange(for: CGFloat.self, of: { geo in
-                    return geo.contentOffset.y + geo.contentInsets.top
-                }, action: { new, old in
-                    offset = new
-                })
-                .ignoresSafeArea(.all, edges: .top)
-                .onChange(of: offset) {
-                    scaleEffect = max(1, 1 + (-offset / reader.size.height * (reader.size.height / 300)))
-                }
+                content
+            }
+            .onScrollGeometryChange(for: CGFloat.self, of: { geo in
+                return geo.contentOffset.y + geo.contentInsets.top
+            }, action: { new, old in
+                offset = new
+            })
+            .ignoresSafeArea(.all, edges: .top)
+            .onChange(of: offset) {
+                scaleEffect = max(1, 1 + (-offset / reader.size.height * (reader.size.height / style.headerHeight)))
+            }
         }
     }
 }
 
-#Preview {
+enum StickyHeaderListStyle {
+    case large
+    case medium
+    case small
+
+    var headerHeight: CGFloat {
+        switch self {
+        case .large:
+            450
+        case .medium:
+            300
+        case .small:
+            200
+        }
+    }
+}
+
+extension EnvironmentValues {
+    @Entry
+    var stickyHeaderListStyle: StickyHeaderListStyle = .large
+}
+
+extension View {
+    func stickyHeaderListStyle(_ style: StickyHeaderListStyle) -> some View {
+        environment(\.stickyHeaderListStyle, style)
+    }
+}
+
+#Preview("Large") {
     StickyHeaderList {
         LinearGradient(colors: [.orange, .pink, .purple], startPoint: .topLeading, endPoint: .bottomTrailing)
     } content: {
@@ -63,4 +97,37 @@ struct StickyHeaderList<Header: View, Content: View>: View {
             Text("Item \(index)")
         }
     }
+}
+
+#Preview("Medium") {
+    StickyHeaderList {
+        LinearGradient(colors: [.orange, .pink, .purple], startPoint: .topLeading, endPoint: .bottomTrailing)
+    } content: {
+        Section {
+            Text("Hello")
+        }
+
+        ForEach(0..<100) { index in
+            Text("Item \(index)")
+        }
+    }
+    .stickyHeaderListStyle(.medium)
+}
+
+#Preview("Small") {
+    Text("Hello")
+        .sheet(isPresented: .constant(true)) {
+            StickyHeaderList {
+                LinearGradient(colors: [.orange, .pink, .purple], startPoint: .topLeading, endPoint: .bottomTrailing)
+            } content: {
+                Section {
+                    Text("Hello")
+                }
+
+                ForEach(0..<100) { index in
+                    Text("Item \(index)")
+                }
+            }
+            .stickyHeaderListStyle(.small)
+        }
 }
