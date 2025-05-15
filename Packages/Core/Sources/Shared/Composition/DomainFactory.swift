75177b62ce030a56f79d60c62bbe83899685dcb0
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-15 16:58:02 +0200

    ✨ Vend user preference storage

---
+++ Packages/Core/Sources/Shared/Composition/DomainFactory.swift
@@ -0,0 +1,24 @@
+//
+//  DomainFactory.swift
+//  Core
+//
+//  Created by Dorian on 15/05/2025.
+//
+
+import Factory
+
+/// A factory that provides shared domains for the app.
+public final class DomainFactory: SharedContainer {
+    public static let shared: DomainFactory = .init()
+    public let manager: ContainerManager = .init()
+
+    /// A date provider that provides the current date.
+    public var dateProvider: Factory<DateProvider> {
+        self { .main }
+    }
+
+    /// A key store that stores user preferences.
+    public var userPreferenceStorage: Factory<KeyStore> {
+        self { .userDefaults(suitePrefix: "dev.skwiggs.newblack-x") }
+    }
+}
