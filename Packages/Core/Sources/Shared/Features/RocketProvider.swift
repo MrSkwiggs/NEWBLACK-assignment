f0a6b9684916d88ae95d5c3088c7a2c958c22fb8
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-15 15:12:20 +0200

    ✨ Create Rocket provider

---
+++ Packages/Core/Sources/Shared/Features/RocketProvider.swift
@@ -0,0 +1,36 @@
+//
+//  RocketProvider.swift
+//  Core
+//
+//  Created by Dorian on 15/05/2025.
+//
+
+import Foundation
+import API
+
+/// A type that can provide rockets
+public protocol RocketProviding: Sendable, Actor {
+    /// Fetches rockets from the API with the given filters.
+    /// - Parameter page: The page number to fetch.
+    /// - Returns: A paginated list of rockets.
+    func fetch(atPage page: Int) async throws -> Paginated<Rocket>
+
+    /// Fetches a rocket by its ID.
+    /// - Parameter rocketID: The ID of the rocket to fetch.
+    /// - Returns: A rocket if found, otherwise nil.
+    func fetch(rocketByID rocketID: String) async throws -> Rocket?
+}
+
+public actor RocketProvider: RocketProviding {
+    public func fetch(
+        atPage page: Int
+    ) async throws -> Paginated<Rocket> {
+        try await API.Rockets
+            .fetchAll(page: page)
+    }
+
+    public func fetch(rocketByID rocketID: String) async throws -> Rocket? {
+        try await API.Rockets
+            .fetch(byID: rocketID)
+    }
+}
