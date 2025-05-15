c1f3a44870615b60611ac616eb0fb2a6394b08b8
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-15 09:58:09 +0200

    ♻️ Decode dates as ISO8601 instead of Unix Timestamps
    This is needed because we can't query date ranges on Unix timestamps

--- Packages/Core/Sources/API/API.swift
+++ Packages/Core/Sources/API/API.swift
@@ -17,8 +17,21 @@ public final class API: Sendable {
 
     package init() {
         let decoder = JSONDecoder()
-        decoder.dateDecodingStrategy = .secondsSince1970
-        self.requestPerformer = RequestPerformer(session: .init(configuration: .ephemeral), decoder: decoder)
+        // The following is needed because it's 2025 and Apple still doesn't know how to decode
+        // ISO8601 dates with fractional seconds 😤
+        decoder.dateDecodingStrategy = .custom { decoder in
+            let isoFormatter = ISO8601DateFormatter()
+            isoFormatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+            let container = try decoder.singleValueContainer()
+            let dateString = try container.decode(String.self)
+            guard let date = isoFormatter.date(from: dateString) else {
+                throw DecodingError.dataCorruptedError(in: container, debugDescription: "Cannot decode ISO8601 date: \(dateString)")
+            }
+            return date
+        }
+        let encoder: JSONEncoder = .init()
+        encoder.dateEncodingStrategy = .iso8601
+        self.requestPerformer = RequestPerformer(session: .init(configuration: .ephemeral), encoder: encoder, decoder: decoder)
     }
 
     package func send<R: Request>(_ request: R) async throws -> R.Response {
