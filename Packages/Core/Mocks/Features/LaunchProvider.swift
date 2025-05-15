5a8716806e086acb507ac549fcca74fbcbf4cd62
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-15 15:53:11 +0200

    ♻️ Use mock handler & duration

--- Packages/Core/Mocks/Features/LaunchProvider.swift
+++ Packages/Core/Mocks/Features/LaunchProvider.swift
@@ -10,18 +10,37 @@ import Shared
 import Foundation
 
 public actor MockLaunchProvider: LaunchProviding {
-    private var replies: [Result<Paginated<Launch>, Error>]
-    private let hookLaunches: (_ page: Int, _ filters: [DateRangeFilter]) -> Void
-    
-    /// Creates a mock provider with the given replies.
+    private var pages: [Result<Paginated<Launch>, Error>]
+
+    /// A closure that is called when `fetch` is called.
+    /// This closure can be used to validate that the parameters passed to the function are correct, or that the function is called.
+    /// It is useful for testing purposes.
     /// - Parameters:
-    ///   - replies: A list of success / failures to return.
-    ///   - hookLaunches: This closure is called during the `fetch` method. It can be used as a hook when performing tests, validating that certain parameters are passed or that the function is called.
+    ///   - page: The page that is being fetched.
+    ///   - filters: The filters that are being applied.
+    ///   - willReturn: The result about to be returned.
+    /// - Returns: The result that will be used and returned by the current call to `fetch`. You can override the result by returning a different value or throwing an error
+    public typealias LaunchesHook = @Sendable (
+        _ page: Int,
+        _ filters: [DateRangeFilter],
+        _ willReturn: Result<Paginated<Launch>, Error>
+    ) -> Result<Paginated<Launch>, Error>
+
+    private let hookLaunches: LaunchesHook
+
+    private let handler: MockHandler
+
+    /// Creates a mock provider with the given pages.
+    /// - Parameters:
+    ///   - pages: A list of success / failures to return.
+    ///   - hookLaunches: This closure is called during the `fetch` method. It can be used as a hook when performing tests, validating that certain parameters are passed or that the function is called. You c
     public init(
-        replies: [Result<Paginated<Launch>, Error>],
-        hookLaunches: @escaping (_ page: Int, _ filters: [DateRangeFilter]) -> Void = { _, _ in }
+        pages: [Result<Paginated<Launch>, Error>],
+        duration: MockDuration = .short,
+        hookLaunches: @escaping LaunchesHook = { _, _, response in response }
     ) {
-        self.replies = replies
+        self.pages = pages
+        self.handler = .init(duration)
         self.hookLaunches = hookLaunches
     }
 
@@ -29,32 +48,36 @@ public actor MockLaunchProvider: LaunchProviding {
         atPage page: Int,
         filters: [DateRangeFilter]
     ) async throws -> Paginated<Launch> {
-        guard !replies.isEmpty else {
-            return .empty()
+        let response = pages.isEmpty ? .success(.empty()) : pages.removeFirst()
+
+        return try await handler { [hookLaunches] in
+            try hookLaunches(page, filters, response).get()
         }
-
-        hookLaunches(page, filters)
-
-        return try replies.removeFirst().get()
     }
 }
 
 public extension MockLaunchProvider {
+
+    /// A mock delay to simulate network latency.
+    static var mockDuration: MockDuration = .short
+
     /// A mock provider that returns an empty list of launches.
-    static let empty: MockLaunchProvider = .init(replies: [])
+    static let empty: MockLaunchProvider = .init(pages: [], duration: mockDuration)
 
     /// A mock provider that returns a list of launches.
     static let success: MockLaunchProvider = .init(
-        replies: [
+        pages: [
             .success(.init(items: [.krakenUnleashed, .minmusMambo], page: 1, pageSize: 2, nextPage: 2)),
             .success(.init(items: [.munaholicAchievement, .seaOfKerbalDebut], page: 2, pageSize: 2, nextPage: nil))
-        ]
+        ],
+        duration: mockDuration
     )
 
     /// A mock provider that returns a failure.
     static let failure: MockLaunchProvider = .init(
-        replies: [
-            .failure(MockError.mock),
-        ]
+        pages: [
+            .failure(MockError()),
+        ],
+        duration: mockDuration
     )
 }
