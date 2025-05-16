650f7235597ccc7d2b58fc5e8ea5a389a2a9cfca
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-16 10:19:11 +0200

    ⚡️ Add conformance to Mockable

--- Packages/Core/Mocks/Entities/Links.swift
+++ Packages/Core/Mocks/Entities/Links.swift
@@ -8,21 +8,30 @@
 import Foundation
 import Entities
 
-public extension Links {
-    static let munaholic = Links(
+extension Links: Mockable {
+    public static let munaholic = Links(
         webcast: URL(string: "https://youtube.com/watch?v=munaholic-success"),
         wikipedia: URL(string: "https://kerbalwiki.com/Munaholic_Achievement")
     )
 
-    static let splashdown = Links(
+    public static let splashdown = Links(
         webcast: URL(string: "https://livestream.com/ksc/splashdown-whoa"),
         wikipedia: nil
     )
 
-    static let kraken = Links(
+    public static let kraken = Links(
         webcast: URL(string: "https://twitch.tv/ksc/kraken-live"),
         wikipedia: URL(string: "https://kerbalwiki.com/Space_Kraken_Unleashed")
     )
 
-    static let none = Links(webcast: nil, wikipedia: nil)
+    public static let none = Links(webcast: nil, wikipedia: nil)
+
+    public static var mocks: [Links] {
+        [
+            munaholic,
+            splashdown,
+            kraken,
+            none
+        ]
+    }
 }
--- Packages/Core/Mocks/Entities/Rockets.swift
+++ Packages/Core/Mocks/Entities/Rockets.swift
@@ -8,8 +8,8 @@
 import Foundation
 import Entities
 
-public extension Rocket {
-    static let kraken: Rocket = .init(
+extension Rocket: Mockable {
+    public static let kraken: Rocket = .init(
         id: UUID().uuidString,
         name: "Kraken",
         type: "Experimental",
@@ -23,7 +23,7 @@ public extension Rocket {
         description: "Meet the Kraken: a gravity-devouring behemoth that rips through orbits with all the subtlety of a barnacle-crusted squid. Equipped with next-gen thrusters and enough cargo space to haul an entire Mun colony—if they survive the ride. Side effects may include spontaneous disassembly and an existential crisis when you realize you just outran your parachute."
     )
 
-    static let falcon9 = Rocket(
+    public static let falcon9 = Rocket(
         id: "R-falcon9",
         name: "Falcon 9 Comical Booster",
         type: "Orbital-class",
@@ -37,7 +37,7 @@ public extension Rocket {
         description: "The trusty Falcon 9: delivering satellites and shattered dreams with pinpoint precision. Features reusable first stage and occasional celebratory landings on drone ships."
     )
 
-    static let starship = Rocket(
+    public static let starship = Rocket(
         id: "R-starship",
         name: "Starship",
         type: "Super Heavy",
@@ -50,4 +50,12 @@ public extension Rocket {
         imageURLs: [URL(string: "https://imgur.com/starship.jpg")!],
         description: "Starship: the stainless-steel giant that’s either our ticket to Mars or a spectacular firework show. Coming soon to an atmosphere near you."
     )
+
+    public static var mocks: [Rocket] {
+        [
+            kraken,
+            falcon9,
+            starship
+        ]
+    }
 }
