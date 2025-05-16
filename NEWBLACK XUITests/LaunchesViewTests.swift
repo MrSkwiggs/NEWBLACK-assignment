6389978974190928b6f23cf52edfccb6e1850323
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-16 13:27:58 +0200

    ✅ Add Launch View tests

--- NEWBLACK XUITests/LaunchesViewTests.swift
+++ NEWBLACK XUITests/LaunchesViewTests.swift
@@ -1,5 +1,5 @@
 //
-//  NEWBLACK_XUITests.swift
+//  LaunchesViewTests.swift
 //  NEWBLACK XUITests
 //
 //  Created by Dorian on 16/05/2025.
@@ -7,14 +7,14 @@
 
 import XCTest
 
-final class NEWBLACK_XUITests: BaseTestCase {
+final class LaunchesViewTests: BaseTestCase {
 
     func testLoadsMorePages() {
-        let launches = navigateToHomePage(loadingDuration: 5).launches
+        let launches = navigateToHomePage(loadingDuration: 3).launches
         XCTAssertTrue(
-            launches.pageLoader.waitForNonExistence(timeout: 4),
+            launches.pageLoader.waitForNonExistence(timeout: 1),
             "Page Loader should disappear during refresh"
         )
-        XCTAssertTrue(launches.pageLoader.waitForExistence(timeout: 4), "Page Loader should exist after initial load")
+        XCTAssertTrue(launches.pageLoader.waitForExistence(timeout: 3), "Page Loader should exist after initial load")
     }
 }
