5f72a883285f8df940bedc875683a7a232077c9f
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-16 11:23:56 +0200

    ⚡️ Provide mocks per state

--- Packages/Core/Mocks/Composition/FeatureFactory.swift
+++ Packages/Core/Mocks/Composition/FeatureFactory.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Shared
+import Entities
 
 public extension FeatureFactory {
     /// Configures the factory to use mock implementations.
@@ -21,6 +22,44 @@ public extension FeatureFactory {
 
         shared.rocketProvider.register { @MainActor in
             MockRocketProvider.success(mockDuration: mockDuration)
+        }
+    }
+
+    static func useMocks(
+        with state: LaunchArgument.State,
+        mockDuration: MockDuration = .oneSecond
+    ) {
+        switch state {
+        case .empty:
+            shared.filterProvider.register { @MainActor in
+                MockFilterProvider.empty
+            }
+            shared.launchProvider.register { @MainActor in
+                MockLaunchProvider.empty(mockDuration: mockDuration)
+            }
+            shared.rocketProvider.register { @MainActor in
+                MockRocketProvider.empty(mockDuration: mockDuration)
+            }
+        case .failure:
+            shared.filterProvider.register { @MainActor in
+                MockFilterProvider.empty
+            }
+            shared.launchProvider.register { @MainActor in
+                MockLaunchProvider.failure(mockDuration: mockDuration)
+            }
+            shared.rocketProvider.register { @MainActor in
+                MockRocketProvider.failure(mockDuration: mockDuration)
+            }
+        case .success:
+            shared.filterProvider.register { @MainActor in
+                MockFilterProvider.two
+            }
+            shared.launchProvider.register { @MainActor in
+                MockLaunchProvider.success(mockDuration: mockDuration)
+            }
+            shared.rocketProvider.register { @MainActor in
+                MockRocketProvider.success(mockDuration: mockDuration)
+            }
         }
     }
 }
