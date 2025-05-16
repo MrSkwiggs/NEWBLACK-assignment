0a4cf9f80c6616f166dbb67723d4ec6ada1040c3
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-16 11:23:39 +0200

    ✨ Create ViewIdentifiers & Provider

---
+++ Packages/Core/Sources/Entities/UI/ViewIdentifiers.swift
@@ -0,0 +1,39 @@
+//
+//  ViewIdentifiers.swift
+//  Core
+//
+//  Created by Dorian on 16/05/2025.
+//
+
+import SwiftUI
+
+/// A base class for view identifiers.
+public final class ViewIdentifiers: Sendable {
+    /// The ID class of a view.
+    open class ID: BaseViewIdentifiers {
+        public init(file: String) {
+            super.init(bundleIdentifier: "PinchAssignment", file: file)
+        }
+
+        public func id(_ id: String) -> String {
+            "\(key)_\(id)"
+        }
+    }
+
+    /// The Main view identifiers.
+    public static let main: ViewIdentifiers = .init()
+}
+
+public extension View {
+    /// Assigns the root accessibility identifier to this view
+    func rootIdentifier<Base: BaseViewIdentifiers>(_ identifer: KeyPath<ViewIdentifiers, Base>) -> some View {
+        self
+            .accessibilityElement(children: .contain)
+            .accessibilityIdentifier(ViewIdentifiers.main[keyPath: identifer].root)
+    }
+
+    /// Assigns the given accessibility identifier to this subview
+    func elementIdentifier(_ identifier: KeyPath<ViewIdentifiers, String>) -> some View {
+        self.accessibilityIdentifier(ViewIdentifiers.main[keyPath: identifier])
+    }
+}
