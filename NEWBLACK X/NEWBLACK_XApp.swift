d05f57e544a89a3503da8970d2e53bca0948686a
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-05-16 11:24:55 +0200

    ♻️ Use ViewModelFactory based on Launch arguments

--- NEWBLACK X/NEWBLACK_XApp.swift
+++ NEWBLACK X/NEWBLACK_XApp.swift
@@ -11,10 +11,55 @@ import Factory
 @main
 struct NEWBLACK_XApp: App {
 
+    @StateObject
+    var viewModelFactory: ViewModelFactory
+
+    init() {
+    #if DEBUG
+        _viewModelFactory = .init(wrappedValue: Self.debugViewModelFactory())
+    #else
+        _viewModelFactory = .init(wrappedValue: ViewModelFactory())
+    #endif
+    }
+
     var body: some Scene {
         WindowGroup {
             HomeView()
                 .environmentObject(ViewModelFactory())
         }
+    }
+}
+
+import Mocks
+import Entities
+
+private extension NEWBLACK_XApp {
+    static func debugViewModelFactory() -> ViewModelFactory {
+        let arguments = LaunchArgumentFactory.shared.launchArguments()
+        guard !arguments.isEmpty else {
+            return ViewModelFactory()
+        }
+
+        let loadDuration = getMockDuration(from: arguments)
+        let state = getState(from: arguments)
+        return .mock(for: state, with: loadDuration)
+    }
+
+    static func getMockDuration(from arguments: [LaunchArgument]) -> MockDuration {
+        for argument in arguments {
+            if case .loadDuration(let duration) = argument {
+                return .seconds(duration)
+            }
+        }
+        return .instant
+    }
+
+    static func getState(from arguments: [LaunchArgument]) -> LaunchArgument.State {
+        for argument in arguments {
+            if case .state(let state) = argument {
+                return state
+            }
+        }
+        return .success
     }
 }
